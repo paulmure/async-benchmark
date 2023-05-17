865bca47d086778c80b246e4a115b1be376f985e
Paul Mure <paulmure2000@gmail.com>
2023-05-17 14:29:57 -0700

    rust

---
+++ rust/src/main.rs
@@ -0,0 +1,35 @@
+use clap::Parser;
+use std::sync::{Arc, Mutex};
+use tokio::{
+    spawn,
+    time::{sleep, Duration},
+};
+
+#[derive(Parser)]
+struct Cli {
+    num_threads: i32,
+}
+
+async fn wait(ctr: Arc<Mutex<i32>>) {
+    sleep(Duration::from_secs(1)).await;
+    let mut num = ctr.lock().unwrap();
+    *num += 1;
+}
+
+#[tokio::main]
+async fn main() {
+    let args = Cli::parse();
+    let counter = Arc::new(Mutex::new(0));
+    let mut handles = vec![];
+
+    for _ in 0..args.num_threads {
+        handles.push(spawn(wait(counter.clone())))
+    }
+
+    futures::future::join_all(handles).await;
+
+    {
+        let num = counter.lock().unwrap();
+        assert_eq!(*num, args.num_threads);
+    }
+}
