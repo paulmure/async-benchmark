0ac7c863baff6ae818ffacc03126366be955c323
Paul Mure <paulmure2000@gmail.com>
2023-05-19 23:49:59 -0700

    add rust mpmc

---
+++ rust/channel-mpmc/src/main.rs
@@ -0,0 +1,35 @@
+use async_io::Timer;
+use clap::Parser;
+use crossbeam::channel::{unbounded, Receiver, Sender};
+use futures::future::join_all;
+use smol::spawn;
+use std::time::{Duration, SystemTime};
+
+#[derive(Parser)]
+struct Cli {
+    num_threads: i32,
+}
+
+async fn wait_inc(tx: Sender<i32>, rx: Receiver<i32>) {
+    tx.send(1).unwrap();
+    Timer::after(Duration::from_secs(1)).await;
+    assert_eq!(rx.recv().unwrap(), 1);
+}
+
+fn main() {
+    let start = SystemTime::now();
+    smol::block_on(async {
+        let args = Cli::parse();
+        let (tx, rx): (Sender<i32>, Receiver<i32>) = unbounded();
+        let mut handles = vec![];
+
+        for _ in 0..args.num_threads {
+            handles.push(spawn(wait_inc(tx.clone(), rx.clone())));
+        }
+
+        join_all(handles).await;
+    });
+    let end = SystemTime::now();
+    let duration = end.duration_since(start).unwrap();
+    println!("{}", duration.as_secs_f64());
+}
