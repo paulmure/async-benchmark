70b7ab488f4bb430a68e4ac60bb4ea2b85a3a906
ZhenbangYou <1800012963@pku.edu.cn>
2023-05-20 22:47:37 -0700

    add mpsc channel in F#

---
+++ fsharp/channel/channel.fsx
@@ -0,0 +1,23 @@
+open System.Threading.Channels
+open System.Threading.Tasks
+
+// mpsc
+let channel (numThreads: int) =
+    let c: Channel<int> = Channel.CreateUnbounded(UnboundedChannelOptions())
+    let r = c.Reader
+    let w = c.Writer
+
+    for _ = 1 to numThreads do
+        task {
+            let! _ = Task.Delay 1000
+            w.WriteAsync(1) |> ignore
+        }
+        |> ignore
+
+    for _ = 1 to numThreads do
+        Task.WaitAll([| r.ReadAsync().AsTask() |]: Task array)
+
+let stopWatch = System.Diagnostics.Stopwatch.StartNew()
+channel (System.Environment.GetCommandLineArgs()[2] |> int)
+stopWatch.Stop()
+printfn "%fs" stopWatch.Elapsed.TotalMilliseconds
